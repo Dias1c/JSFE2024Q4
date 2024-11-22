906a76a5ff4803ea4ea1ba0826fdc3ceb3de9618
Dias1c <dias.kappasov@gmail.com>
2024-11-22 20:22:55 +0500

    feat: rendering all gifts in gifts page

--- christmas-shop/pages/gifts/script.js
+++ christmas-shop/pages/gifts/script.js
@@ -2,7 +2,7 @@ import { initialRenderGifts } from "./../../scripts/gifts.js";
 
 let gifts = [];
 
-fetch("./gifts.json").then(async (resp) => {
+fetch("./../../assets/data/all_gifts.json").then(async (resp) => {
   gifts = JSON.parse(await resp.text());
 
   initialRenderGifts({ gifts });
