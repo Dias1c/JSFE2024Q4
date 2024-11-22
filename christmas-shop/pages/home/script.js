2d589ee8f74613fd5adfe68de5bba58635c9e913
Dias1c <dias.kappasov@gmail.com>
2024-11-22 20:15:55 +0500

    feat: added random gifts generations

--- christmas-shop/pages/home/script.js
+++ christmas-shop/pages/home/script.js
@@ -1,9 +1,18 @@
 import { initialRenderGifts } from "./../../scripts/gifts.js";
 
+const BEST_GIFTS_COUNT = 4;
+
 let gifts = [];
 
-fetch("./gifts.json").then(async (resp) => {
-  gifts = JSON.parse(await resp.text());
+fetch("./../../assets/data/all_gifts.json").then(async (resp) => {
+  let all = JSON.parse(await resp.text());
+
+  for (let i = 0; i < BEST_GIFTS_COUNT && i < all.length; i++) {
+    const randIdx = Math.round(Math.random() * (all.length - 1));
+    const gift = all[randIdx];
+    all.splice(randIdx, 1);
+    gifts.push(gift);
+  }
 
   initialRenderGifts({ gifts });
 });
