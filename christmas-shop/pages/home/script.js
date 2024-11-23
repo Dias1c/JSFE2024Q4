44da543672543e71bf18e70beb7a625dfbc06bf6
Dias1c <dias.kappasov@gmail.com>
2024-11-23 22:02:15 +0500

    feat: handle timer

--- christmas-shop/pages/home/script.js
+++ christmas-shop/pages/home/script.js
@@ -1,4 +1,5 @@
 import { reRenderGifts } from "./../../scripts/gifts.js";
+import { getNewYearRemains } from "./../../scripts/new-year-timer.js";
 
 const BEST_GIFTS_COUNT = 4;
 
@@ -17,3 +18,18 @@ fetch("./../../assets/data/all_gifts.json").then(async (resp) => {
 
   reRenderGifts({ gifts, elementId: "replace-gifts" });
 });
+
+// ? Setting timer
+const elTimerDays = document.getElementById("new_year_timer_days");
+const elTimerHours = document.getElementById("new_year_timer_hours");
+const elTimerMinutes = document.getElementById("new_year_timer_minutes");
+const elTimerSeconds = document.getElementById("new_year_timer_seconds");
+
+setInterval(() => {
+  const { days, hours, minutes, seconds } = getNewYearRemains({ year: "2025" });
+
+  if (elTimerDays) elTimerDays.innerText = days;
+  if (elTimerHours) elTimerHours.innerText = hours;
+  if (elTimerMinutes) elTimerMinutes.innerText = minutes;
+  if (elTimerSeconds) elTimerSeconds.innerText = seconds;
+}, 1000);
