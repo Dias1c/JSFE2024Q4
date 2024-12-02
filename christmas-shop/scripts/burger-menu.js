7432f5c90e47d73a7cd9ea167590f0541bbce143
Dias1c <dias.kappasov@gmail.com>
2024-12-02 16:34:20 +0500

    feat: add burger menu script

---
+++ christmas-shop/scripts/burger-menu.js
@@ -0,0 +1,67 @@
+const openBurgerMenu = ({ elementButtonBurger, elementSectionMenu }) => {
+  elementButtonBurger.classList.add("active");
+  elementSectionMenu.classList.add("active");
+  document.body.style.overflow = "hidden";
+};
+
+const closeBurgerMenu = ({ elementButtonBurger, elementSectionMenu }) => {
+  elementButtonBurger.classList.remove("active");
+  elementSectionMenu.classList.remove("active");
+  document.body.style.overflow = "auto";
+};
+
+const handleClickBurgerButton = ({
+  elementButtonBurger,
+  elementSectionMenu,
+}) => {
+  const isActive = elementButtonBurger.classList.contains("active");
+  if (isActive) {
+    closeBurgerMenu({
+      elementButtonBurger,
+      elementSectionMenu,
+    });
+    return;
+  }
+  openBurgerMenu({
+    elementButtonBurger,
+    elementSectionMenu,
+  });
+};
+
+export const initializeBurgerMenuHandlers = ({
+  elementButtonBurger,
+  elementSectionMenu,
+  elementsMenuItem,
+}) => {
+  if (!elementButtonBurger)
+    throw new Error(`There is no elementButtonBurger: ${elementButtonBurger}`);
+  if (!elementSectionMenu)
+    throw new Error(`There is no elementSectionMenu: ${elementSectionMenu}`);
+  if (!elementsMenuItem)
+    throw new Error(`There is no elementsMenuItem: ${elementsMenuItem}`);
+
+  Array.from(elementsMenuItem).forEach((elementMenuItem) => {
+    elementMenuItem.addEventListener("click", () => {
+      closeBurgerMenu({
+        elementButtonBurger,
+        elementSectionMenu,
+      });
+    });
+  });
+
+  elementButtonBurger.addEventListener("click", () => {
+    handleClickBurgerButton({
+      elementButtonBurger,
+      elementSectionMenu,
+    });
+  });
+
+  const observerMaxWidth = window.matchMedia("(max-width: 768px)");
+
+  observerMaxWidth.addEventListener("change", () => {
+    closeBurgerMenu({
+      elementButtonBurger,
+      elementSectionMenu,
+    });
+  });
+};
